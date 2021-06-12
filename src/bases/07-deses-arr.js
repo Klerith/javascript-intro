eaea7618e0343e558be32df7ad6ba5dd7ad07113
Fernando Herrera <fernando.herrera85@gmail.com>
2021-06-12 14:40:07 -0400

    Bases creadas

---
+++ src/bases/07-deses-arr.js
@@ -0,0 +1,20 @@
+
+
+const personajes = ['Goku','Vegeta','Trunks'];
+const [ , , p3 ] = personajes;
+console.log( p3 );
+
+
+const retornaArreglo = () =>{
+    return ['ABC', 123];
+}
+
+const [ letras, numeros ] = retornaArreglo(); 
+console.log(letras, numeros);
+
+
+
+
+
+
+
---
+++ src/bases/12-ternario.js
@@ -0,0 +1,19 @@
+
+
+const activo = true;
+
+// let mensaje = '';
+
+// if ( !activo ) {
+//     mensaje = 'Activo';
+// } else {
+//     mensaje = 'Inactivo';
+// }
+// const mensaje = ( activo ) ? 'Activo' : 'Inactivo'; 
+// const mensaje = ( activo ) ? 'Activo' : null; 
+const mensaje = activo && 'Activo';
+
+
+console.log(mensaje);
+
+
